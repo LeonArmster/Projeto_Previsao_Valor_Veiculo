7e0d01c3b62af6e4f8972823b7f4614bdd2bd490
LeonArmster <leo.armster1994@gmail.com>
2026-02-26 23:50:53 -0300

    Modelo de previsao de precos de veiculos

---
+++ Code/config.py
@@ -0,0 +1,8 @@
+# Biblioteca
+from pathlib import Path
+
+# Definindo o caminho padrão da pastapara a pastas
+caminho = Path(__file__).resolve().parent.parent
+
+# Caminho para a pasta data
+data = caminho/'Data'
